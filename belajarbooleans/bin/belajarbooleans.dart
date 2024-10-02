b58598b6acba18af39fe2bf1f963c0f91e5899a6
Faridbhp <farid.bhp9431@gmail.com>
2024-10-02 11:40:46 +0700

    create belajarbooleans

---
+++ belajarbooleans/bin/belajarbooleans.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: dead_code
+
+void main() {
+  if (true) {
+    print("It's true");
+  } else {
+    print("It's False");
+  }
+}
+
+/// Output:
+/// It's true
---
+++ belajarbooleans/test/belajarbooleans_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajarbooleans/belajarbooleans.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
