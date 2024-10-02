bcd308921940743d2e417ebb2b9a77be0d8383a0
Faridbhp <farid.bhp9431@gmail.com>
2024-10-02 11:34:36 +0700

    create belajarstring

---
+++ belajarstring/bin/belajarstring.dart
@@ -0,0 +1,9 @@
+// void main() {
+//   var name = 'Messi';
+//   print('Hello $name, nice to meet you.');
+//   print('1 + 1 = ${1 + 1}');
+// }
+
+void main() {
+  print(r'Dia baru saja membeli komputer seharga $1,000.00'); // untuk menampilkan harga dolar maka tambahkan r
+}
